4ea2215f2062ea8d0c01842bb1f8e5106be935d1
smack0007 <zachary.snow@gmail.com>
2018-05-17 22:46:43 +0200

    Added cmtk and a few aliases.

--- alias.cmd
+++ alias.cmd
@@ -1,2 +1,4 @@
 DOSKEY alias=notepad %~dp0\%0
+DOSKEY cd=cd /D $*
 DOSKEY ls=dir /B
+DOSKEY pwd=echo %%cd%%
--- init.cmd
+++ init.cmd
@@ -5,10 +5,13 @@ CALL alias.cmd
 
 SET PATH=%PATH%;%~dp0\functions
 
+IF EXIST %~dp0\cmdtk\cmdtk.exe (
+    %~dp0\cmdtk\cmdtk.exe opacity 90
+)
+
 POPD
 
+:: This has to be the last command otherwise execution will appear to stop
 IF EXIST %~dp0\clink\clink.bat (
     %~dp0\clink\clink.bat inject
 )
-
-@ECHO ON
