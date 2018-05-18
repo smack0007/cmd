53424def593a919f5a4bffaf9245f52533f32ae6
smack0007 <zachary.snow@gmail.com>
2018-05-18 14:25:02 +0200

    Removed window size stuff from install

--- install.cmd
+++ install.cmd
@@ -1,4 +1,1 @@
 reg add "HKCU\Software\Microsoft\Command Processor" /v AutoRun /t REG_EXPAND_SZ /d "%~dp0init.cmd" /f
-
-:: Set the default size
-reg add "HKCU\Console" /v WindowSize /t REG_DWORD /d 00190050 /f
