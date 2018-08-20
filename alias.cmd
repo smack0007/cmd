89d27e04eee96c88a179fd04b56ab847cb53fdcd
smack0007 <zachary.snow@gmail.com>
2018-08-20 09:23:45 +0200

    Removed gitex alias

--- alias.cmd
+++ alias.cmd
@@ -3,7 +3,6 @@ DOSKEY cd=cd /D $* ^&^& "%~dp0\scripts\cmd-update-title.cmd"
 DOSKEY clear=cls
 DOSKEY cp=copy $*
 DOSKEY e.=explorer .
-DOSKEY gitex=gitextensions
 DOSKEY git-log=git log --oneline --all --graph --decorate  $*
 DOSKEY grep=findstr $*
 DOSKEY ls=dir /B
