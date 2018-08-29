919973a0d3a9337d13b3a55ecb5f6470043c1a7e
smack0007 <zachary.snow@gmail.com>
2018-08-29 16:18:20 +0200

    Add csc command

--- init.cmd
+++ init.cmd
@@ -6,7 +6,7 @@ CALL env.cmd
 
 SET BIN_DIR=%~dp0bin\
 SET SCRIPTS_DIR=%~dp0scripts\
-SET PATH=%PATH%;%~dp0bin;%~dp0scripts
+SET PATH=%~dp0bin;%~dp0scripts;%PATH%
 
 IF EXIST clink\clink.bat (
     CALL clink\clink.bat inject --quiet
