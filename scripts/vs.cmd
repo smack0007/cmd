86ab6cf8d207d4fc90da2b952cabda7fe7e696fd
smack0007 <zachary.snow@gmail.com>
2018-08-31 18:24:06 +0200

    Add vs.cmd

---
+++ scripts/vs.cmd
@@ -0,0 +1,7 @@
+@ECHO OFF
+
+IF EXIST "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\Common7\IDE\devenv.exe" (
+    START "" "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\Common7\IDE\devenv.exe" %*
+) ELSE (
+    ECHO Visual Studio not found.
+)
