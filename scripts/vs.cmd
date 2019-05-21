991e1daa8b93a72d265f39a291f5ea546acfe297
smack0007 <zachary.snow@gmail.com>
2019-05-21 09:00:56 +0200

    Updated vs.cmd to 2019

--- scripts/vs.cmd
+++ scripts/vs.cmd
@@ -1,9 +1,9 @@
 @ECHO OFF
 
-IF EXIST "C:\Program Files (x86)\Microsoft Visual Studio\2017\Professional\Common7\IDE\devenv.exe" (
-    START "" "C:\Program Files (x86)\Microsoft Visual Studio\2017\Professional\Common7\IDE\devenv.exe" %*
-) ELSE IF EXIST "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\Common7\IDE\devenv.exe" (
-    START "" "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\Common7\IDE\devenv.exe" %*
+IF EXIST "C:\Program Files (x86)\Microsoft Visual Studio\2019\Professional\Common7\IDE\devenv.exe" (
+    START "" "C:\Program Files (x86)\Microsoft Visual Studio\2019\Professional\Common7\IDE\devenv.exe" %*
+) ELSE IF EXIST "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\Common7\IDE\devenv.exe" (
+    START "" "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\Common7\IDE\devenv.exe" %*
 ) ELSE (
     ECHO Visual Studio not found.
 )
