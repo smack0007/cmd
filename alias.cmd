167425649e22a9e630d7d5d0e880e7dfd8661f24
smack0007 <zachary.snow@gmail.com>
2020-02-11 10:17:54 +0100

    Added dir macro

--- alias.cmd
+++ alias.cmd
@@ -2,6 +2,7 @@ DOSKEY alias=npp "%~dp0\%0"
 DOSKEY cd=cd /D $* ^&^& "%~dp0\scripts\cmd-update-title.cmd"
 DOSKEY clear=cls
 DOSKEY cp=copy $*
+DOSKEY dir=dir /A $*
 DOSKEY e.=explorer .
 DOSKEY git-acp=git add -A ^&^& git commit -m $* ^&^& git push origin
 DOSKEY git-log=git log --oneline --all --graph --decorate  $*
