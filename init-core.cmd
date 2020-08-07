a4ade4c7f10aab4b4fdf9183790f392e4d3edfdf
smack0007 <zachary.snow@gmail.com>
2020-08-07 14:09:33 +0200

    Changed prompt character from > to #

--- init-core.cmd
+++ init-core.cmd
@@ -8,7 +8,7 @@ SET BIN_DIR=%~dp0bin\
 SET SCRIPTS_DIR=%~dp0scripts\
 SET PATH=%~dp0bin;%~dp0scripts;%PATH%
 
-SET PROMPT=$E[m$E[32m$E]$E\$E[92m$P$E[90m$_$E[90m$G$E[m$S$E]9;12$E\
+SET PROMPT=$E[m$E[32m$E]$E\$E[92m$P$E[90m$_$E[90m#$E[m$S$E]9;12$E\
 
 POPD
 
