6da0997b875b613af4f18f76e534c56d226e3899
smack0007 <zachary.snow@gmail.com>
2020-08-18 09:19:08 +0200

    Using global @types from npm-prefix in tsexec.cmd

--- scripts/tsexec.cmd
+++ scripts/tsexec.cmd
@@ -1,7 +1,15 @@
 @ECHO OFF
+SETLOCAL enableextensions 
+
+REM Get the location of the the npm-prefix directory.
+FOR /f "tokens=*" %%a in ( 'npm config get prefix' ) DO ( SET __NPM_PREFIX=%%a ) 
+REM Remove trailing space from npm-prefix
+SET __NPM_PREFIX=%__NPM_PREFIX:~0,-1%
 
 REM Get all arguments except the first.
 SET __SCRIPTARGS=
 FOR /f "tokens=1,* delims= " %%a IN ("%*") DO ( SET __SCRIPTARGS=%%b )
 
-tsc --strict --typeRoots "%~dp0\..\tsc\@types" --outFile %1.js %1 && node %1.js %__SCRIPTARGS%
+tsc --strict --typeRoots "%__NPM_PREFIX%\node_modules\@types" --outFile %1.js %1 && node %1.js %__SCRIPTARGS%
+
+ENDLOCAL
