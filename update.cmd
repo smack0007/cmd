dc893e6f746a4f414a98920271b20758505f07c8
smack0007 <zachary.snow@gmail.com>
2020-11-24 14:03:59 +0100

    Improved update.cmd

--- update.cmd
+++ update.cmd
@@ -2,10 +2,10 @@
 TITLE %cd%
 
 SET __GITBRANCH=
-for /f %%I in ('git.exe rev-parse --abbrev-ref HEAD 2^> NUL') do set __GITBRANCH=%%I
+FOR /f %%I IN ('git branch --show-current 2^> NUL') DO SET __GITBRANCH=%%I
 
-if "%__GITBRANCH%" == "" (
+IF "%__GITBRANCH%" == "" (
     PROMPT %__PROMPT_PRE__%%__PROMPT_POST__%
-) else (
+) ELSE (
     PROMPT %__PROMPT_PRE__% $E[94m%__GITBRANCH%$E[90m%__PROMPT_POST__%
 )
