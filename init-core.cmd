ca5082051c6fb7499493080bc4fe305929e78d8c
smack0007 <zachary.snow@gmail.com>
2020-11-24 16:08:52 +0100

    Added time to prompt and refactored prompt

--- init-core.cmd
+++ init-core.cmd
@@ -8,8 +8,18 @@ SET BIN_DIR=%~dp0bin\
 SET SCRIPTS_DIR=%~dp0scripts\
 SET PATH=%~dp0bin;%~dp0scripts;%PATH%
 
-SET __PROMPT_PRE__=$E[m$E[32m$E]$E\$E[92m$P$E[90m
-SET __PROMPT_POST__=$_$E[90m#$E[m$S$E]9;12$E\
+REM $E[92m Green foreground
+REM $P Current drive and path
+REM $E[97m White foreground
+REM [$T$H$H$H$H$H$H] Time with brackets
+REM $_ Newline
+REM $E[90m Gray foreground
+REM # Number sign
+REM $S Space
+REM $E[37m Light gray foreground
+
+SET __PROMPT_PRE__=$E[92m$P
+SET __PROMPT_POST__= $E[97m[$T$H$H$H$H$H$H]$_$E[90m#$S$E[37m
 PROMPT %__PROMPT_PRE__%%__PROMPT_POST__%
 
 POPD
--- update.cmd
+++ update.cmd
@@ -4,8 +4,12 @@ TITLE %cd%
 SET __GITBRANCH=
 FOR /f %%I IN ('git branch --show-current 2^> NUL') DO SET __GITBRANCH=%%I
 
+REM $E[94m Blue foreground
+REM $C (
+REM $F )
+
 IF "%__GITBRANCH%" == "" (
     PROMPT %__PROMPT_PRE__%%__PROMPT_POST__%
 ) ELSE (
-    PROMPT %__PROMPT_PRE__% $E[94m%__GITBRANCH%$E[90m%__PROMPT_POST__%
+    PROMPT %__PROMPT_PRE__% $E[94m$C%__GITBRANCH%$F%__PROMPT_POST__%
 )
