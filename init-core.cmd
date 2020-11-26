7816e5b85c8aacb5f04d4b14694c619a5e3660da
smack0007 <zachary.snow@gmail.com>
2020-11-26 10:15:34 +0100

    Show seconds in prompt

--- init-core.cmd
+++ init-core.cmd
@@ -11,7 +11,7 @@ SET PATH=%~dp0bin;%~dp0scripts;%PATH%
 REM $E[92m Green foreground
 REM $P Current drive and path
 REM $E[97m White foreground
-REM [$T$H$H$H$H$H$H] Time with brackets
+REM [$T$H$H$H] Time with brackets
 REM $_ Newline
 REM $E[90m Gray foreground
 REM # Number sign
@@ -19,7 +19,7 @@ REM $S Space
 REM $E[37m Light gray foreground
 
 SET __PROMPT_PRE__=$E[92m$P
-SET __PROMPT_POST__= $E[97m[$T$H$H$H$H$H$H]$_$E[90m#$S$E[37m
+SET __PROMPT_POST__= $E[97m[$T$H$H$H]$_$E[90m#$S$E[37m
 PROMPT %__PROMPT_PRE__%%__PROMPT_POST__%
 
 POPD
