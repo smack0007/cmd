5aeb6b0ffcce229a358b9a374e0b31b9ae46439d
smack0007 <zachary.snow@gmail.com>
2020-11-30 15:35:09 +0100

    Added status indicator for git branches.

--- update.cmd
+++ update.cmd
@@ -4,6 +4,14 @@ TITLE %cd%
 SET __GITBRANCH=
 FOR /f %%I IN ('git branch --show-current 2^> NUL') DO SET __GITBRANCH=%%I
 
+SET __GITSTATUS=
+IF NOT "%__GITBRANCH%" == "" (
+    FOR /f %%I IN ('git status -s 2^> NUL') DO SET __GITSTATUS=%%I
+)
+IF NOT "%__GITSTATUS%" == "" (
+    SET __GITSTATUS=*
+)
+
 REM $E[94m Blue foreground
 REM $C (
 REM $F )
@@ -11,5 +19,5 @@ REM $F )
 IF "%__GITBRANCH%" == "" (
     PROMPT %__PROMPT_PRE__%%__PROMPT_POST__%
 ) ELSE (
-    PROMPT %__PROMPT_PRE__% $E[94m$C%__GITBRANCH%$F%__PROMPT_POST__%
+    PROMPT %__PROMPT_PRE__% $E[94m$C%__GITBRANCH%%__GITSTATUS%$F%__PROMPT_POST__%
 )
