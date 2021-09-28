8cdb8b63060723309efe484f99e0e8afe31093db
smack0007 <zachary.snow@gmail.com>
2021-09-28 16:01:16 +0200

    msys2.cmd should now work with VS Code

--- msys2.cmd
+++ msys2.cmd
@@ -3,4 +3,7 @@
 REM Use the same "HOME" directory as Windows
 SET HOME=%USERPROFILE%
 
-CALL C:\msys64\msys2_shell.cmd -defterm -here -no-start -mingw64 -use-full-path
+SET __MSYS_CMD=C:\msys64\msys2_shell.cmd
+SET __MSYS_ARGS=-defterm -here -no-start -mingw64 -use-full-path
+
+CALL %__MSYS_CMD% %__MSYS_ARGS%
--- msys2_vscode.cmd
+++
@@ -1,2 +0,0 @@
-@ECHO OFF
-CALL C:\msys64\msys2_shell.cmd -defterm -here -no-start -mingw64 -use-full-path -c "cd /d; exec bash"
