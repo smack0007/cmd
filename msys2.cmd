37227392ea8f792490fa36375eaa297ee1d5d818
smack0007 <zachary.snow@gmail.com>
2021-10-03 13:47:46 +0200

    Pass args in msys2.cmd

--- msys2.cmd
+++ msys2.cmd
@@ -10,4 +10,4 @@ SET HOME=%USERPROFILE%
 SET __MSYS_CMD=C:\msys64\msys2_shell.cmd
 SET __MSYS_ARGS=-defterm -here -no-start -mingw64 -use-full-path
 
-CALL %__MSYS_CMD% %__MSYS_ARGS%
+CALL %__MSYS_CMD% %__MSYS_ARGS% %*
