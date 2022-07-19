0343871a1c11e02bd0ca240ad37fbfdb04bdb856
Zachary Snow <zachary.snow@gmail.com>
2022-07-19 20:41:35 +0200

    Adding aliases for fedora

--- wsl/fedora/.zshrc
+++ wsl/fedora/.zshrc
@@ -1,1 +1,3 @@
 CMDROOT=$( cd "${0:a:h}/../.." &> /dev/null && pwd )
+
+source ${0:a:h}/aliases.sh
---
+++ wsl/fedora/aliases.sh
@@ -0,0 +1,26 @@
+alias cmd-cd="cd $CMDROOT"
+alias cmd-code="code $CMDROOT"
+alias cmd-explorer="pushd $CMDROOT > /dev/null && explorer.exe . && popd > /dev/null"
+alias cmd-gitex="pushd $CMDROOT > /dev/null && gitex && popd > /dev/null"
+alias cmd-pull="pushd $CMDROOT > /dev/null && git pull && popd > /dev/null"
+
+function cmd-push() {
+    pushd $CMDROOT > /dev/null
+    git add -A && git commit -m "$1" && git push origin master
+    popd > /dev/null
+}
+
+alias cmd-status="pushd $CMDROOT > /dev/null && git status -s && popd > /dev/null"
+
+alias e="explorer.exe"
+
+alias fork="wt.exe -w 0 -d '$(pwd)'"
+
+alias gitex="cmd.exe /c '\"C:\Program Files (x86)\GitExtensions\gitex.cmd\"'"
+
+alias ll="ls -lA --color=auto"
+
+function mcd() {
+    mkdir -p $1
+    cd $1
+}
