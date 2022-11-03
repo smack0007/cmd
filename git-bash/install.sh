6c14e714713e530dcd44fa3768c61d163b3f7753
smack0007 <zachary.snow@gmail.com>
2022-11-03 08:25:40 +0100

    .gitconfig.aliases is now just .gitconfig (2)

--- git-bash/install.sh
+++ git-bash/install.sh
@@ -1,3 +1,2 @@
 echo '. /c/cmd/git-bash/.bashrc' >> ~/.bash_profile
-git config --global init.defaultBranch main
-git config --global include.path "/c/cmd/git/.gitconfig.aliases"
+git config --global include.path "/c/cmd/git/.gitconfig"
